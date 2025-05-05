0b7b706a30895d5165f94982f3fe4e4a01cf2858
holmesalee90 <hamzaali1.cis@gmail.com>
2025-05-04 22:03:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 document.getElementById("absenceForm").addEventListener("submit", async (e) => {
     e.preventDefault();
-    const studentId = document.getElementById("studentId").value;
-    const reason = document.getElementById("reason").value;
-    
-    // TEST: Verify form works
-    console.log("Submitting:", studentId, reason); 
-    alert("Form connected! Next step: Azure Function.");
+    const response = await fetch(
+        "https://uni-attendance-api.azurewebsites.net/api/ProcessAbsence?code=k4E62ZAkEayZc2mVjq85eGwtONaGOkFsJM1FcLK6OvDrAzFuwiGa1g==",
+        {
+            method: "POST",
+            body: JSON.stringify({
+                studentId: document.getElementById("studentId").value,
+                reason: document.getElementById("reason").value
+            }),
+            headers: { "Content-Type": "application/json" }
+        }
+    );
+    alert(await response.text());
 });
